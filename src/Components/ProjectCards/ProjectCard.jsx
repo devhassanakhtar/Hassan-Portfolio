52779ff1adec1274494332510d849414f8af464d
devhassanakhtar <hcodex5@gmail.com>
2026-04-15 16:32:51 +0500

    Upload all my projects to the portfolio

--- src/Components/ProjectCards/ProjectCard.jsx
+++ src/Components/ProjectCards/ProjectCard.jsx
@@ -6,6 +6,15 @@ import Project2 from "../../assets/Project2.webp";
 import Project3 from "../../assets/Project3.webp";
 import Project4 from "../../assets/Project4.webp";
 import Project5 from "../../assets/Project5.webp";
+import Project6 from "../../assets/Project6.webp";
+import Project7 from "../../assets/Project7.webp";
+import Project8 from "../../assets/Project8.webp";
+import Project9 from "../../assets/Project9.webp";
+import Project10 from "../../assets/Project10.webp";
+import Project11 from "../../assets/Project11.webp";
+import Project12 from "../../assets/Project12.webp";
+import Project13 from "../../assets/Project13.webp";
+import { NavLink } from "react-router";
 
 const ProjectCard = (props) => {
   const projectCard = [
@@ -36,20 +45,68 @@ const ProjectCard = (props) => {
     {
       id: 5,
       ProductImage: Project5,
-      PrductHeading: "Xian Gallers Portfolio",
-      ProductDescription: "2024 November",
+      PrductHeading: "Add To Cart",
+      ProductDescription: "2026 February",
+    },
+    {
+      id: 6,
+      ProductImage: Project6,
+      PrductHeading: "Age Calculator",
+      ProductDescription: "2026 March",
+    },
+    {
+      id: 7,
+      ProductImage: Project7,
+      PrductHeading: "To-Do-List",
+      ProductDescription: "2026 March",
+    },
+    {
+      id: 8,
+      ProductImage: Project8,
+      PrductHeading: "Calculator",
+      ProductDescription: "2025 December",
+    },
+    {
+      id: 9,
+      ProductImage: Project9,
+      PrductHeading: "Currency Converter",
+      ProductDescription: "2025 December",
+    },
+    {
+      id: 10,
+      ProductImage: Project10,
+      PrductHeading: "Login & Registration Form",
+      ProductDescription: "2025 December",
+    },
+    {
+      id: 11,
+      ProductImage: Project11,
+      PrductHeading: "Password Generator",
+      ProductDescription: "2025 December",
+    },
+    {
+      id: 12,
+      ProductImage: Project12,
+      PrductHeading: "Rock Paper Scissor",
+      ProductDescription: "2025 December",
+    },
+    {
+      id: 13,
+      ProductImage: Project13,
+      PrductHeading: "Tic Tac Toe",
+      ProductDescription: "2025 December",
     }
   ];
 
   const displayedProjects = props.limit
-  ? projectCard.slice(0, props.limit)
-  : projectCard;
+    ? projectCard.slice(0, props.limit)
+    : projectCard;
 
   return (
     <div className="Project__Card">
       <div className="Project__Heading">
         <h2>My Projects</h2>
-        <span>See All</span>
+        <span><NavLink to="/work"> See All</NavLink></span>
       </div>
       <div className="Project__Section">
         {displayedProjects.map(function (elem) {
